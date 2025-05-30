effcc9fd1dc64a8c822bbb9d752c2e0216587357
Ashish Shah <eiepixxstuff@gmail.com>
2025-05-31 01:55:46 +0530

    feat: add inngest event driven pipeline to find user and send an email

---
+++ ai-ticket-assistant/src/inngest/client.js
@@ -0,0 +1,4 @@
+import { Inngest } from "inngest";
+
+const inngest = new Inngest({ id: "ticketing-system" });
+export default inngest;
---
+++ ai-ticket-assistant/src/inngest/inngest-utils/on-signup.inngest.js
@@ -0,0 +1,36 @@
+import sendMail from "../../utils/mailer.util.js";
+import inngest from "../client.js";
+import User from "../../models/user.model.js";
+import { NonRetriableError } from "inngest";
+
+export const onUserSignUp = inngest.createFunction(
+    { id: "on-user-signup", retries: 2 },
+    { event: "user/signup" },
+    async ({ event, step }) => {
+        try {
+            const { email } = event.data;
+            const user = await step.run("get-user-email", async () => {
+                const userObject = await User.findOne({ email });
+                if (!userObject) {
+                    throw new NonRetriableError("User doesn't exist.");
+                }
+
+                return userObject;
+            });
+
+            await step.run("send-welcome-email", async () => {
+                const subject = "Welcome to AI app";
+                const signupMessage = `Hey there,
+                \n\n
+                Thanks for signing up. we're glad to have you onboard!
+                `;
+                await sendMail(user.email, subject, signupMessage);
+            });
+
+            return { success: true };
+        } catch (error) {
+            console.error(`❌ Error running signup step ${error}`);
+            return { success: false };
+        }
+    },
+);
