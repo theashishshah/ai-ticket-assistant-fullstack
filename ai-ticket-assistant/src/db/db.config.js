389f77e0fa65cce740faf07229e9923d3ac9798b
Ashish Shah <eiepixxstuff@gmail.com>
2025-05-31 00:37:14 +0530

    feat(db): add database connection model

---
+++ ai-ticket-assistant/src/db/db.config.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const dbConnect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            serverSelectionTimeoutMS: 5000,
+            socketTimeoutMS: 45000,
+            autoIndex: false,
+            maxPoolSize: 10,
+        });
+
+        console.log("✅ Database connected successfully.");
+    } catch (error) {
+        console.log(process.env.MONGODB_URI);
+        console.error("❌ Database connection error:", error.message);
+        process.exit(1);
+    }
+};
+
+mongoose.connection.on("disconnected", () => {
+    console.warn(`⚠️  MongoDB database connection disconnected. Retrying...`);
+    setTimeout(dbConnect, 5 * 1000);
+});
+
+process.on("SIGINT", async function () {
+    await mongoose.connection.close();
+    console.warn(`⚠️  Mongodb connection closed due to Ctrl + C press.`);
+    process.exit(0);
+});
+
+export default dbConnect;
