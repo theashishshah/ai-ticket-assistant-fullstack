4eb07f24d929e2961ae2abd226a435e5d86236bb
Ashish Shah <eiepixxstuff@gmail.com>
2025-05-31 16:56:52 +0530

    feat: add inngest middleware and script to run inngest

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,10 @@ import cors from "cors";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import ticketRoutes from "./routes/ticket.route.js";
+import { serve } from "inngest/express";
+import { inngest } from "./inngest/client.js";
+import { onUserSignUp } from "./inngest/inngest-utils/on-signup.inngest.js";
+import { onTicketCreated } from "./inngest/inngest-utils/on-ticket-create.inngest.js";
 
 const app = express();
 
@@ -11,8 +15,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", (req, res) => res.send("You're on home page."));
-app.use("/auth", authRoutes);
-app.use("/users", userRoutes);
-app.use("/tickets", ticketRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/tickets", ticketRoutes);
+app.use(
+    "/apiinngest",
+    serve({
+        client: inngest,
+        functions: [onTicketCreated, onUserSignUp],
+    }),
+);
 
 export default app;
