8934a7d65e4ea73a6b4b04b35bdaf0c7a54d2bc6
Ashish Shah <eiepixxstuff@gmail.com>
2025-05-31 18:50:41 +0530

    feat: add structures of pages and done checking auth in frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,57 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CheckAuth from "./components/CheckAuth.jsx";
+import Tickets from "./pages/Tickets.jsx";
+import TicketDetailsPage from "./pages/Ticket.jsx";
+import Login from "./pages/Login.jsx";
+import Singup from "./pages/Singup.jsx";
+import Admin from "./pages/Admin.jsx";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         <BrowserRouter>
             <Routes>
-                <Route path="/" />
+                <Route
+                    path="/"
+                    element={
+                        <CheckAuth protected={true}>
+                            <Tickets />
+                        </CheckAuth>
+                    }
+                />
+                <Route
+                    path="/tickets/:id"
+                    element={
+                        <CheckAuth protected={true}>
+                            <TicketDetailsPage />
+                        </CheckAuth>
+                    }
+                />
+                <Route
+                    path="/login"
+                    element={
+                        <CheckAuth protected={false}>
+                            <Login />
+                        </CheckAuth>
+                    }
+                />
+                <Route
+                    path="/singup"
+                    element={
+                        <CheckAuth protected={false}>
+                            <Singup />
+                        </CheckAuth>
+                    }
+                />
+                <Route
+                    path="/admin"
+                    element={
+                        <CheckAuth protected={true}>
+                            <Admin />
+                        </CheckAuth>
+                    }
+                />
             </Routes>
         </BrowserRouter>
     </StrictMode>,
